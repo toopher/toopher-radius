e1ea30f01c33856534c49c6261ea0341b63d0f0c
Drew Shafer <drew@toopher.com>
2014-01-27 16:05:58 -0600

    Updates for windows installer

--- cygwin/installed_scripts/debug_toopher_radius.cmd
+++ cygwin/installed_scripts/debug_toopher_radius.cmd
@@ -1,3 +1,20 @@
+@echo off
+echo *******************************************
+echo ** DEBUG MODE - NOT FOR PRODUCTION USE!  **
+echo *******************************************
+echo.
+echo You are about to start the Toopher-RADIUS server in DEBUG mode, which will
+echo cause a large amount of information to be logged to the console, including
+echo low-level RADIUS protocol information.  Because most RADIUS clients use a
+echo plaintext authentication mechanism, this is a potential security risk.
+echo.
+echo POTENTIALLY SENSITIVE INFORMATION, INCLUDING USER PASSWORDS, MAY BE DISPLAYED
+echo ON THE CONSOLE WHEN RUNNING THIS SCRIPT.  THIS SCRIPT SHOULD NOT BE RUN IN A
+echo PRODUCTION ENVIRONMENT.
+echo.
+echo Press [Enter] to continue starting the server, or [Ctrl-C] to abort
+pause
+
 set CYGWIN=nodosfilewarning
 call test_config.cmd
 if ERRORLEVEL 1 goto :eof
---
+++ cygwin/installed_scripts/reset_user_pairing.cmd
@@ -0,0 +1,3 @@
+set CYGWIN=nodosfilewarning
+set PERL_LWP_SSL_CA_FILE=%~dp0\etc\raddb\Mozilla\CA\cacert.pem
+sbin\perl etc\raddb\toopher_radius.pl reset-pairing
